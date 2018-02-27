37722a5aaa20574363d038433ffa31d54a53b41a
Libor Vašíček <libor.vasicek@braiins.cz>
2018-02-27 18:56:29 +0100

    Rotate firmware partition for system upgrade
    - old partition is preserved for automatic rescue when current
      update is not successful
    - MTD partitions are named firmware1 and firmware2
    - U-Boot firmware env variable is updated accordingly to boot from
      the correct partition

--- target/linux/zynq/base-files/lib/upgrade/platform.sh
+++ target/linux/zynq/base-files/lib/upgrade/platform.sh
@@ -1,5 +1,22 @@
 REQUIRE_IMAGE_METADATA=1
 
+RAMFS_COPY_BIN="/usr/sbin/fw_printenv /usr/sbin/fw_setenv"
+RAMFS_COPY_DATA="/etc/fw_env.config /var/lock/fw_printenv.lock"
+
+zynq_get_firmware_partition() {
+	cur_firmware=$(fw_printenv -n firmware)
+	partition_name=""
+	if [ x"$cur_firmware" = x"2" ]; then
+		partition_name="firmware1"
+		fw_setenv firmware 1
+	elif [ x"$cur_firmware" = x"1" ]; then
+		partition_name="firmware2"
+		fw_setenv firmware 2
+	fi
+
+	echo "$partition_name"
+}
+
 platform_check_image() {
 	return 0;
 }
@@ -9,5 +26,5 @@ platform_pre_upgrade() {
 }
 
 platform_nand_pre_upgrade() {
-	CI_UBIPART="firmware1"
+	CI_UBIPART="$(zynq_get_firmware_partition)"
 }
