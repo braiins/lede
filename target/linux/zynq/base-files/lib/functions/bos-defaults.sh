291558adcf52c0993bf7231c44d6f71dbf10f27f
Libor Vašíček <libor.vasicek@braiins.cz>
2019-01-11 16:24:28 +0100

    Use system hostname for DHCP client
    - fix miner utility to use system hostname when DHCP client does not
      override it
    - move 'get_config' function from '02_network' to 'bos-defaults.sh' and
      prefix it with bos_ (it can be used as an library)
    - set system hostname instead of eth0 hostname

---
+++ target/linux/zynq/base-files/lib/functions/bos-defaults.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+BOS_MODE=$(cat /etc/bos_mode)
+
+bos_get_config() {
+	# return empty string when bOS mode is not 'nand' to use default settings
+	case $BOS_MODE in
+	'nand')
+		/usr/sbin/fw_printenv -n "$1" 2>/dev/null
+		;;
+	'recovery')
+		/usr/sbin/fw_printenv -c /tmp/fw_env.config -n "recovery_$1" 2>/dev/null
+		;;
+	esac
+}
