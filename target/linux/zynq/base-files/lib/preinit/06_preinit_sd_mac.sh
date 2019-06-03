83fb7edcb2beaa8b82362c6c61435563b6fd9d37
Libor Vašíček <libor.vasicek@braiins.com>
2019-06-03 15:55:20 +0200

    Add MAC address random generator to preinit script #2287
    - use 'bos' utility to recover MAC address from factory configuration
    - generate MAC address when all source of MAC address failed; this is
      done in Linux startup where can be used pseudo random generator
      (U-Boot use time for seed and it generates the same MAC all the time)

--- target/linux/zynq/base-files/lib/preinit/06_preinit_sd_mac.sh
+++ target/linux/zynq/base-files/lib/preinit/06_preinit_sd_mac.sh
@@ -18,7 +18,25 @@ get_new_mac() {
 	fi
 
 	# try to get MAC address from NAND from bOS environment variable
-	fw_printenv -n "ethaddr" 2>/dev/null
+	local new_mac=$(fw_printenv -n "ethaddr" 2>/dev/null)
+	# it is also stored in factory configuration environment variable
+	[ -n "$new_mac" ] || new_mac=$(/usr/sbin/bos get_factory_cfg "ethaddr" 2>/dev/null)
+
+	if [ -z "$new_mac" ]; then
+		# generate new MAC when all source failed
+		new_mac=$(dd if=/dev/urandom bs=1024 count=1 2>/dev/null | /usr/bin/md5sum | \
+			sed 's/^\(..\)\(..\)\(..\)\(..\)\(..\)\(..\).*$/\1:\2:\3:\4:\5:\6/')
+
+		# generate a random Ethernet address (MAC) that is not multicast
+		# and bit 1 (local) is set
+		local firstbyte=$(echo "$new_mac" | cut -d: -f 1)
+		local lastfive=$(echo "$new_mac" | cut -d: -f 2-6)
+
+		firstbyte=$(printf '%02x' $(( 0x$firstbyte & 254 | 2)))
+		new_mac="$firstbyte:$lastfive"
+	fi
+
+	echo "$new_mac"
 	return
 }
 
