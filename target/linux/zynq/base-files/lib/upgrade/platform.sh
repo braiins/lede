ce6cde43d12e5e821ae62079131185160520d47e
Libor Vašíček <libor.vasicek@braiins.com>
2020-03-19 23:30:07 +0100

    Add support of partial upgrades from full images
    - add 'EXTERNAL_BINARY_TREE' configuration for specification of
      path to external directory with prebuild bootloaders (SPL, U-Boot,
      U-Boot default environment)
    - add generic function 'zynq_write_if_different' which writes image
      to NAND only if the content of image differs from current NAND
      content
    - add to 'zynq_write_uboot' upgrade of U-Boot default environment
      when U-Boot is upgraded
    - refactor 'zynq_get_firmware_partition' to use fw_setenv script
      instead of multiple commands/writes to NAND
    - add function 'fix_recovery_partition' to 'COMMAND' sysupgrade
      script for fixing previous firmwares where original U-Boot image
      is not stored in recovery partition (this image is used during
      factory reset to restoring original U-Boot)
    - create sysupgrade image from prebuild bootloaders to ensure
      binary compatibility which is needed for comparison of current
      bootloaders in NAND

--- target/linux/zynq/base-files/lib/upgrade/platform.sh
+++ target/linux/zynq/base-files/lib/upgrade/platform.sh
@@ -1,35 +1,61 @@
 REQUIRE_IMAGE_METADATA=1
 
-RAMFS_COPY_BIN="/bin/busybox:/bin/ln:/bin/sed:/usr/bin/tail"
+RAMFS_COPY_BIN="/bin/busybox:/bin/ln:/bin/sed:/bin/gzip:/usr/bin/head:/usr/bin/tail"
 RAMFS_COPY_BIN="$RAMFS_COPY_BIN /usr/sbin/nanddump"
 RAMFS_COPY_BIN="$RAMFS_COPY_BIN /usr/sbin/fw_printenv"
 RAMFS_COPY_BIN="$RAMFS_COPY_BIN /usr/sbin/fw_printenv:/usr/sbin/fw_setenv"
 
 RAMFS_COPY_DATA="/etc/fw_env.config /var/lock/fw_printenv.lock"
 
-zynq_write_spl() {
+zynq_write_if_different() {
 	local tar_file=$1
+	local file_name=$2
+	local mtd_name=$3
+	local msg=$4
 	local board_name="$(nand_board_name)"
 
-	local spl_length=`(tar xf ${tar_file} sysupgrade-$board_name/spl -O | wc -c) 2> /dev/null`
+	local file_path=sysupgrade-$board_name/$file_name
+	local file_length=`(tar xf ${tar_file} $file_path -O | wc -c) 2>/dev/null`
 
-	[ "$spl_length" != 0 ] && {
-		echo "Upgrading SPL..."
-		mtd erase boot
-		tar xf $tar_file sysupgrade-$board_name/spl -O | mtd write - boot
-	}
+	[ "$file_length" != 0 ] || return 2
+
+	local tmp_file="/tmp/$file_name"
+	tar xf ${tar_file} $file_path -O > "$tmp_file"
+
+	local result=0
+	if [ $(mtd verify "$tmp_file" ${mtd_name} 2>&1 1>/dev/null | tail -n 1) != "Success" ];	then
+		echo "Upgrading $msg..."
+		mtd erase ${mtd_name}
+		mtd write "$tmp_file" ${mtd_name}
+	else
+		echo "Skipping upgrade of $msg..."
+		result=1
+	fi
+
+	rm "$tmp_file"
+	return $result
+}
+
+zynq_write_spl() {
+	local tar_file=$1
+
+	zynq_write_if_different ${tar_file} spl boot "SPL"
 }
 
 zynq_write_uboot() {
 	local tar_file=$1
 	local board_name="$(nand_board_name)"
 
-	local uboot_length=`(tar xf ${tar_file} sysupgrade-$board_name/uboot -O | wc -c) 2> /dev/null`
+	zynq_write_if_different ${tar_file} uboot uboot "U-Boot" || return 0
 
-	[ "$uboot_length" != 0 ] && {
-		echo "Upgrading U-Boot..."
-		mtd erase uboot
-		tar xf $tar_file sysupgrade-$board_name/uboot -O | mtd write - uboot
+	# it is also needed to upgrade U-Boot enviroment when U-Boot has been upgraded
+	local file_path=sysupgrade-$board_name/uenv
+	local uenv_length=`(tar xf ${tar_file} $file_path -O | wc -c) 2>/dev/null`
+
+	[ "$uenv_length" != 0 ] && {
+		echo "Upgrading U-Boot default environment..."
+		tar xf ${tar_file} $file_path -O | fw_setenv --script -
+		fw_printenv
 	}
 }
 
@@ -37,17 +63,13 @@ zynq_write_fpga_partition() {
 	local tar_file=$1
 	local cur_partition_name="fpga$2"
 	local dst_partition_name="fpga$3"
-	local board_name="$(nand_board_name)"
-	local cur_mtd="/dev/mtd$(($2 + 1))"
-	local cur_fpga="/tmp/cur_fpga.bin"
 
-	local fpga_length=`(tar xf ${tar_file} sysupgrade-$board_name/fpga -O | wc -c) 2> /dev/null`
+	zynq_write_if_different ${tar_file} fpga ${dst_partition_name} "FPGA bitstream"
+	local result=$?
+	if [ $result -eq 2 ]; then
+		local cur_mtd="/dev/mtd$(($2 + 1))"
+		local cur_fpga="/tmp/cur_fpga.bin"
 
-	if [ "$fpga_length" != 0 ]; then
-		echo "Upgrading FPGA bitstream..."
-		mtd erase ${dst_partition_name}
-		tar xf $tar_file sysupgrade-$board_name/fpga -O | mtd write - ${dst_partition_name}
-	else
 		# dump current FPGA bitstream and compare it with destination one
 		# copy current partition to destination one only if it differs
 		nanddump ${cur_mtd} > "$cur_fpga"
@@ -64,20 +86,22 @@ zynq_get_firmware_partition() {
 	local dst_firmware=$1
 	local partition_name="firmware$1"
 
-	# remove factory reset to prevent collision with upgrade
-	fw_setenv factory_reset
-
-	# boot after upgrade is treated as a first one; this prevents interference
-	# with inserted SD card where uEnv.txt can hinder the upgrade process
-	fw_setenv first_boot yes
-
-	# set upgrade stage to 0 to inform U-Boot that upgrade process has been
-	# started; when first boot after upgrade is not successful then U-Boot
-	# reverts firmware to previous one
-	fw_setenv upgrade_stage 0
-
-	# switch to new firmware in environment
-	fw_setenv firmware ${dst_firmware}
+	fw_setenv --script - <<-EOF
+		# remove factory reset to prevent collision with upgrade
+		factory_reset
+		#
+		# boot after upgrade is treated as a first one; this prevents interference
+		# with inserted SD card where uEnv.txt can hinder the upgrade process
+		first_boot yes
+		#
+		# set upgrade stage to 0 to inform U-Boot that upgrade process has been
+		# started; when first boot after upgrade is not successful then U-Boot
+		# reverts firmware to previous one
+		upgrade_stage 0
+		#
+		# switch to new firmware in environment
+		firmware ${dst_firmware}
+	EOF
 
 	echo "$partition_name"
 }
@@ -88,11 +112,13 @@ zynq_command_check_image() {
 	# refuse upgrade when the firmware is not compatible
 	local args="$1"
 	local board_name="$(cat /tmp/sysinfo/board_name)"
-	local command_length=`(get_image "$args" | tar xf - sysupgrade-$board_name/COMMAND -O | wc -c) 2> /dev/null`
 	local command_file="/tmp/sysupgrade-COMMAND"
 
+	local command_path=sysupgrade-$board_name/COMMAND
+	local command_length=`(get_image "$args" | tar xf - $command_path -O | wc -c) 2> /dev/null`
+
 	[ "$command_length" != 0 ] && {
-		get_image "$args" | tar xf - sysupgrade-$board_name/COMMAND -O > "$command_file"
+		get_image "$args" | tar xf - $command_path -O > "$command_file"
 		source "$command_file"
 		if type 'check_image' >/dev/null 2>/dev/null; then
 			check_image "$board_name" "$args" || return 1
@@ -110,10 +136,11 @@ zynq_command_pre_upgrade() {
 	local board_name="$(nand_board_name)"
 	local command_file="/tmp/sysupgrade-COMMAND"
 
-	local command_length=`(tar xf ${tar_file} sysupgrade-$board_name/COMMAND -O | wc -c) 2> /dev/null`
+	local command_path=sysupgrade-$board_name/COMMAND
+	local command_length=`(tar xf ${tar_file} $command_path -O | wc -c) 2> /dev/null`
 
 	[ "$command_length" != 0 ] && {
-		tar xf $tar_file sysupgrade-$board_name/COMMAND -O > "$command_file"
+		tar xf ${tar_file} $command_path -O > "$command_file"
 		source "$command_file"
 		if type 'pre_upgrade' >/dev/null 2>/dev/null; then
 			pre_upgrade "$board_name" "$tar_file"
@@ -200,6 +227,7 @@ platform_nand_pre_upgrade() {
 	zynq_command_pre_upgrade "$1"
 	zynq_write_spl "$1"
 	zynq_write_uboot "$1"
+
 	zynq_write_fpga_partition "$1" ${cur_firmware} ${dst_firmware}
 	CI_UBIPART="$(zynq_get_firmware_partition ${dst_firmware})"
 
